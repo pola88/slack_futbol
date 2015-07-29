b0a5e278d9f2e4cc5194624d0aa8bc0660b0293f
pola88 <pola88@gmail.com>
2015-07-29 11:59:06 -0300

    add express

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 require("babel/register");
-
+var express = require("express");
 var dotenv = require('dotenv');
 dotenv.load();
 
 if(!process.env.PBOT_APITOKEN) {
   throw new Error('Missing Api Token');
 }
+
+var app = express()
+var port = process.env.PORT || 3000
+
+app.get('/', function(req, res) {
+  res.send("What's up?");
+});
+
+app.listen(port);
 
 var run = require('./run').start();
