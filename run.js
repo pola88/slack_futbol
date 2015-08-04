35747f1ca8f57ee4eeab150eb16ef4e6638cef57
pola88 <pola88@gmail.com>
2015-08-04 12:57:44 -0300

    POST /api/time to send time

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 import Connection from './lib/connection';
 import request from "request-promise";
 
-exports.start = function() {
+exports.start = function(callback) {
   let apiToken = process.env.PBOT_APITOKEN;
   let slackApi = process.env.SLACK_API;
   let authUrl = `${slackApi}/rtm.start?token=${apiToken}`;
@@ -13,6 +13,8 @@ exports.start = function() {
                 if (res.ok) {
                   GLOBAL.__ID__ = 1;
                   connection = new Connection(res.url);
+
+                  callback(connection);
                 }
           }, error => {
             console.log(error);
