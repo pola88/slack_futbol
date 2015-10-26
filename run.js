a79fac9df082d8dfaad04e4b82759f7771e75ed1
pola88 <pola88@gmail.com>
2015-10-26 10:18:29 -0300

    runs every Monday at 11:00

--- run.js
+++ run.js
@@ -1,5 +1,6 @@
 import Connection from './lib/connection';
 import request from "request-promise";
+import cron from "./cron";
 
 exports.start = function(callback) {
   let apiToken = process.env.PBOT_APITOKEN;
@@ -9,12 +10,14 @@ exports.start = function(callback) {
   let connection;
   request(authUrl)
          .then( body => {
-                let res = JSON.parse(body);
-                if (res.ok) {
-                  connection = new Connection(res.url);
+            let res = JSON.parse(body);
+            if (res.ok) {
+              connection = new Connection(res.url);
 
-                  callback(connection);
-                }
+              cron.start(connection, function() {
+                callback(connection);
+              });
+            }
           }, error => {
             console.log(error);
             return;
