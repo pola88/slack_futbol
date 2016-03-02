7f78499c30b8049820502914fb72cf33a47f9f38
pola88 <pola88@gmail.com>
2016-03-02 14:32:44 -0300

    add colors team

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("babel/register");
+// Include the logger module
 var dotenv = require('dotenv');
 dotenv.load();
 
@@ -9,5 +10,23 @@ if(!process.env.PBOT_APITOKEN) {
 var port = process.env.PORT || 3001
 
 require('./run').start( function(connection) {
+  var winston = require('winston');
+  // Set up log file. (you can also define size, rotation etc.)
+  winston.add(winston.transports.File, { filename: './logs/app.log', datePattern: '.yyyy-MM-ddTHH-mm', maxFiles: 1, maxsize: 1024 });
+  winston.handleExceptions(new winston.transports.File({
+    filename: './logs/exceptions.log',
+    handleExceptions: true,
+    humanReadableUnhandledException: true,
+    maxFiles: 2,
+    maxsize: 1024
+  }));
+  // Overwrite some of the build-in console functions
+  console.error=winston.error;
+  console.log=winston.info;
+  console.info=winston.info;
+  console.debug=winston.debug;
+  console.warn=winston.warn;
+
+  // console.log(__dirname);
   console.log("Gronpola is running");
 });
